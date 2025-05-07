fd5a965f5eb79707d94a93e844f69fe875e94207
Kirill <kbelous@gmail.com>
2025-05-07 12:08:49 +0300

    create delete loan endpoint

--- src/api/v1/loans.py
+++ src/api/v1/loans.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import APIRouter, HTTPException
 from sqlalchemy.future import select
 from src.database import async_session_factory as async_session
 from src.models import Loan
@@ -7,22 +6,32 @@ from src.schemas.v1.schemas import LoanCreate, LoanOut
 
 router = APIRouter(prefix="/loans", tags=["Loans"])
 
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
 
-@router.post("/", response_model=LoanOut)
-async def create_loan(loan: LoanCreate, session: AsyncSession = Depends(get_session)):
-    new_loan = Loan(**loan.dict())
-    session.add(new_loan)
-    await session.commit()
-    await session.refresh(new_loan)
-    return new_loan
+@router.post("/", response_model=dict)
+async def create_loan(loan: LoanCreate):
+    async with async_session() as session:
+        new_loan = Loan(**loan.dict())
+        session.add(new_loan)
+        await session.commit()
+        await session.refresh(new_loan)
+    return {'message': "Loan created successfully"}
 
 @router.get("/{loan_id}", response_model=LoanOut)
-async def get_loan(loan_id: int, session: AsyncSession = Depends(get_session)):
-    result = await session.execute(select(Loan).where(Loan.id == loan_id))
-    loan = result.scalar_one_or_none()
-    if not loan:
-        raise HTTPException(status_code=404, detail="Loan not found")
+async def get_loan(loan_id: int):
+    async with async_session() as session:
+        result = await session.execute(select(Loan).where(Loan.id == loan_id))
+        loan = result.scalar_one_or_none()
+        if not loan:
+            raise HTTPException(status_code=404, detail="Loan not found")
     return loan
+
+@router.delete("/{loan_id}", response_model=dict)
+async def delete_loan(loan_id: int):
+    async with async_session() as session:
+        result = await session.execute(select(Loan).filter(Loan.id == loan_id))
+        loan = result.scalars().first()
+
+        if loan:
+            await session.delete(loan)
+            await session.commit()
+    return {"message": f"Loan with id {loan_id} has been deleted"}
