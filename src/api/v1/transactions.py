5e1e69d7d7431115f8c9a331f87c7f53eda1ed6f
Kirill <kbelous@gmail.com>
2025-05-11 11:03:05 +0300

    Delete unnecessary transaction endpoint comments

--- src/api/v1/transactions.py
+++ src/api/v1/transactions.py
@@ -11,8 +11,6 @@ from src.schemas.v1.transaction_schema import (TransactionCreate,
 from typing import List
 from src.models import Client
 from src.services.transaction_service import (create_transaction_db,
-                                              get_transactions_by_card_db,
-                                              get_transactions_by_account_db,
                                               account_have_transaction)
 
 router = APIRouter(
@@ -39,21 +37,3 @@ async def get_transaction(
     transaction = await account_have_transaction(transaction_id, current_client.account.id, session)
     return TransactionOut.model_validate(transaction)
 
-# # TODO came up with name of that endpoint
-# @router.get("/account/me", response_model=List[TransactionOut])
-# async def get_transactions_by_account(
-#     current_client: Client = Depends(get_current_client),
-#     session: AsyncSession = Depends(get_db)
-# ) -> List[TransactionOut]:
-#     transactions = await get_transactions_by_account_db(current_client.account.id, session)
-#     return [TransactionOut.model_validate(transaction) for transaction in transactions]
-#
-#
-# @router.get("/card/{card_id}", response_model=List[TransactionOut])
-# async def get_transactions_by_card_id(
-#     card_id: int,
-#     current_client: Client = Depends(get_current_client),
-#     session: AsyncSession = Depends(get_db)
-# ) -> List[TransactionOut]:
-#     transactions = await get_transactions_by_card_db(card_id, current_client.account.id, session)
-#     return [TransactionOut.model_validate(transaction) for transaction in transactions]
